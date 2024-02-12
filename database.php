535f9a3df4170dc4c43f23b59056c6db62cf1c5f
koloseki <koloseki2002@gmail.com>
2024-02-12 22:02:19 +0100

    db password test

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $host = 'j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
 $dbname = 'xrqa3g94vww6t2ea';
 $username = 'iro3c926p8yp03m7';
-$password = ' m5u2m1s0wz8s4afb ';
+$password = 'm5u2m1s0wz8s4afb';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
